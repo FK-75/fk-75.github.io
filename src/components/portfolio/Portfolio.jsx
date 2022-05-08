6797b7d35dae5570b71e67d63ca073debb80669e
FK-75 <farid_khan75@outlook.com>
2022-05-08 19:28:12 +0100

    portfolio section layout completed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,6 +7,45 @@ import {IMG4} from '../../assets/portfolio1.png'
 import {IMG5} from '../../assets/portfolio1.png'
 import {IMG6} from '../../assets/portfolio1.png'
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Image Noise Remover',
+    github: 'https://github.com/FK-75/Image-Noise-Remover'
+  },
+  {
+    id: 2,
+    image: IMG1,
+    title: 'Image Noise Remover',
+    github: 'https://github.com/FK-75/Image-Noise-Remover'
+  },
+  {
+    id: 3,
+    image: IMG1,
+    title: 'Image Noise Remover',
+    github: 'https://github.com/FK-75/Image-Noise-Remover'
+  },
+  {
+    id: 4,
+    image: IMG1,
+    title: 'Image Noise Remover',
+    github: 'https://github.com/FK-75/Image-Noise-Remover'
+  },
+  {
+    id: 5,
+    image: IMG1,
+    title: 'Image Noise Remover',
+    github: 'https://github.com/FK-75/Image-Noise-Remover'
+  },
+  {
+    id: 6,
+    image: IMG1,
+    title: 'Image Noise Remover',
+    github: 'https://github.com/FK-75/Image-Noise-Remover'
+  }
+]
+
 const Portfolio = () => {
   return (
     <section id="portfolio">
@@ -14,22 +53,22 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/" className="btn" target="_blank">Github</a>
-            {/* <a href="https://github.com/" className="btn btn-primary">Live Demo</a> */}
-          </div>
-        </article>
-        
-
-
-
-     
+        {
+          data.map(({id, image, title, github}) => {
+            return (
+              <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target="_blank">Github</a>
+                {/* <a href="https://github.com/" className="btn btn-primary">Live Demo</a> */}
+              </div>
+            </article>
+            )
+          })
+        }
       </div>
     </section>
   )
