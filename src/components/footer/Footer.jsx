ddff04a48068b62fd9fa03268ba1f72af78c67a6
FK-75 <farid_khan75@outlook.com>
2022-05-09 01:00:49 +0100

    main site done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin, BsDiscord} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">Farid Khan</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/faridmkhan/" target="_blank"><BsLinkedin /></a>
+        <a href="https://github.com/FK-75" target="_blank"><FaGithub /></a>
+        <a href="http://discord.com/users/760082645438169139" target="_blank"><BsDiscord /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Farid Khan. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Farid Khan</h1>
-        <h5 className="text-light">FullStack Developer</h5>
+        <h5 className="text-light">Full Stack Developer</h5>
         <CTA />
         <HeaderSocials />
 
